3580850f400c8c3d39e85d40bfa13d4bdfa4281c
Tenko <37594893+Tenkoni@users.noreply.github.com>
2019-05-11 03:37:26 -0500

    Archivo con rueda de la fortuna
    Se borrará cuando se haya unificado el código.

---
+++ main_prac07.cpp
@@ -0,0 +1,412 @@
+﻿/*---------------------------------------------------------*/
+/* ----------------   Práctica 7 --------------------------*/
+/*-----------------    2019-2   ---------------------------*/
+/*------------- Alumno:                     ---------------*/
+#include "esfera.h"
+#include "toroide.h"
+#include "camera.h"
+#include <vector>
+
+
+Esfera my_sphere(1.0);
+Toroide my_torus(1.0, 1.0, 3.0);
+
+void resize(GLFWwindow* window, int width, int height);
+void my_input(GLFWwindow *window);
+void mouse_callback(GLFWwindow *window, double xpos, double ypos);
+void scroll_callback(GLFWwindow *window, double xoffset, double yoffset);
+
+// settings
+// Window size
+int SCR_WIDTH = 3800;
+int SCR_HEIGHT = 7600;
+
+GLFWmonitor *monitors;
+GLuint VBO, VAO, lightVAO;
+
+//Camera
+Camera camera(glm::vec3(0.0f, 0.0f, 3.0f));
+double	lastX = 0.0f,
+lastY = 0.0f;
+bool firstMouse = true;
+
+//Timing
+double	deltaTime = 0.0f,
+lastFrame = 0.0f;
+
+//Lighting
+glm::vec3 lightPos(1.2f, 1.0f, 2.0f);
+
+void myData(void);
+void display(void);
+void getResolution(void);
+void animate(void);
+
+//For Keyboard
+float	movX = 0.0f,
+movY = 0.0f,
+movZ = -5.0f,
+rotX = 0.0f;
+
+float	fortuna_rot = 0.0f,
+year = 0.0f,
+day = 0.0f,
+moon = 0.0f,
+mars_year = 0.0f,
+jupiter_year = 0.0f;
+
+std::vector<float> planets(8);
+std::vector < glm::mat4 > savestate(15);
+std::vector < glm::mat4 > savestate_adorno(15);
+
+
+void getResolution()
+{
+	const GLFWvidmode * mode = glfwGetVideoMode(glfwGetPrimaryMonitor());
+
+	SCR_WIDTH = mode->width;
+	SCR_HEIGHT = (mode->height) - 80;
+
+	lastX = SCR_WIDTH / 2.0f;
+	lastY = SCR_HEIGHT / 2.0f;
+
+}
+
+void myData()
+{
+	GLfloat vertices[] = {
+		//Position				//Normals
+		-0.5f, -0.5f, -0.5f,  0.0f,  0.0f, -1.0f,//
+		 0.5f, -0.5f, -0.5f,  0.0f,  0.0f, -1.0f,
+		 0.5f,  0.5f, -0.5f,  0.0f,  0.0f, -1.0f,
+		 0.5f,  0.5f, -0.5f,  0.0f,  0.0f, -1.0f,
+		-0.5f,  0.5f, -0.5f,  0.0f,  0.0f, -1.0f,
+		-0.5f, -0.5f, -0.5f,  0.0f,  0.0f, -1.0f,
+
+		-0.5f, -0.5f,  0.5f,  0.0f,  0.0f,  1.0f,
+		 0.5f, -0.5f,  0.5f,  0.0f,  0.0f,  1.0f,
+		 0.5f,  0.5f,  0.5f,  0.0f,  0.0f,  1.0f,
+		 0.5f,  0.5f,  0.5f,  0.0f,  0.0f,  1.0f,
+		-0.5f,  0.5f,  0.5f,  0.0f,  0.0f,  1.0f,
+		-0.5f, -0.5f,  0.5f,  0.0f,  0.0f,  1.0f,
+
+		-0.5f,  0.5f,  0.5f, -1.0f,  0.0f,  0.0f,
+		-0.5f,  0.5f, -0.5f, -1.0f,  0.0f,  0.0f,
+		-0.5f, -0.5f, -0.5f, -1.0f,  0.0f,  0.0f,
+		-0.5f, -0.5f, -0.5f, -1.0f,  0.0f,  0.0f,
+		-0.5f, -0.5f,  0.5f, -1.0f,  0.0f,  0.0f,
+		-0.5f,  0.5f,  0.5f, -1.0f,  0.0f,  0.0f,
+
+		 0.5f,  0.5f,  0.5f,  1.0f,  0.0f,  0.0f,
+		 0.5f,  0.5f, -0.5f,  1.0f,  0.0f,  0.0f,
+		 0.5f, -0.5f, -0.5f,  1.0f,  0.0f,  0.0f,
+		 0.5f, -0.5f, -0.5f,  1.0f,  0.0f,  0.0f,
+		 0.5f, -0.5f,  0.5f,  1.0f,  0.0f,  0.0f,
+		 0.5f,  0.5f,  0.5f,  1.0f,  0.0f,  0.0f,
+
+		-0.5f, -0.5f, -0.5f,  0.0f, -1.0f,  0.0f,
+		 0.5f, -0.5f, -0.5f,  0.0f, -1.0f,  0.0f,
+		 0.5f, -0.5f,  0.5f,  0.0f, -1.0f,  0.0f,
+		 0.5f, -0.5f,  0.5f,  0.0f, -1.0f,  0.0f,
+		-0.5f, -0.5f,  0.5f,  0.0f, -1.0f,  0.0f,
+		-0.5f, -0.5f, -0.5f,  0.0f, -1.0f,  0.0f,
+
+		-0.5f,  0.5f, -0.5f,  0.0f,  1.0f,  0.0f,
+		 0.5f,  0.5f, -0.5f,  0.0f,  1.0f,  0.0f,
+		 0.5f,  0.5f,  0.5f,  0.0f,  1.0f,  0.0f,
+		 0.5f,  0.5f,  0.5f,  0.0f,  1.0f,  0.0f,
+		-0.5f,  0.5f,  0.5f,  0.0f,  1.0f,  0.0f,
+		-0.5f,  0.5f, -0.5f,  0.0f,  1.0f,  0.0f
+	};
+
+	glGenVertexArrays(1, &VAO);
+	glBindVertexArray(VAO);
+
+	glGenBuffers(1, &VBO);
+
+	glBindBuffer(GL_ARRAY_BUFFER, VBO);
+	glBufferData(GL_ARRAY_BUFFER, sizeof(vertices), vertices, GL_STATIC_DRAW);
+
+	// position attribute
+	glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 6 * sizeof(float), (void*)0);
+	glEnableVertexAttribArray(0);
+
+	// Normal attribute
+	glVertexAttribPointer(1, 3, GL_FLOAT, GL_FALSE, 6 * sizeof(float), (void*)(3 * sizeof(float)));
+	glEnableVertexAttribArray(1);
+
+
+	//To configure Second Objecto to represet Light
+	glGenVertexArrays(1, &lightVAO);
+	glBindVertexArray(lightVAO);
+
+	glBindBuffer(GL_ARRAY_BUFFER, VBO);
+	glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 6 * sizeof(float), (void*)0);
+	glEnableVertexAttribArray(0);
+
+}
+
+void animate(void)
+{
+	fortuna_rot += 0.2f; //nucleus
+	//planets.at(0) += 0.05; //orbit1
+	//planets.at(1) += 0.07f; //orbit2
+	//planets.at(2) += 0.10f; //orbit3
+	//planets.at(3) += 0.12f; //orbit3
+}
+
+void display(void)
+{
+	//Shader myShader("shaders/shader.vs", "shaders/shader.fs");
+	Shader projectionShader("shaders/shader_projection.vs", "shaders/shader_projection.fs");
+	//Shader projectionShader("shaders/shader_light.vs", "shaders/shader_light.fs");
+	//Shader projectionShader("shaders/shader_light_Gouraud.vs", "shaders/shader_light_Gouraud.fs");
+	Shader lampShader("shaders/shader_lamp.vs", "shaders/shader_lamp.fs");
+
+	//To Use Lighting
+	projectionShader.use();
+	projectionShader.setVec3("lightColor", 1.0f, 1.0f, 1.0f);
+	projectionShader.setVec3("lightPos", lightPos);
+
+	// create transformations and Projection
+	glm::mat4 model = glm::mat4(1.0f);		// initialize Matrix, Use this matrix for individual models
+	glm::mat4 view = glm::mat4(1.0f);		//Use this matrix for ALL models
+	glm::mat4 projection = glm::mat4(1.0f);	//This matrix is for Projection
+
+	//Use "projection" to inlcude Camera
+	projection = glm::perspective(glm::radians(camera.Zoom), (float)SCR_WIDTH / (float)SCR_HEIGHT, 0.1f, 100.0f);
+	view = camera.GetViewMatrix();
+
+	// pass them to the shaders}
+	projectionShader.setVec3("viewPos", camera.Position);
+	projectionShader.setMat4("model", model);
+	projectionShader.setMat4("view", view);
+	// note: currently we set the projection matrix each frame, but since the projection matrix rarely changes it's often best practice to set it outside the main loop only once.
+	projectionShader.setMat4("projection", projection);
+
+
+	glBindVertexArray(VAO);
+	//Soporte1
+	model = savestate.at(0) = glm::translate(savestate.at(0) = glm::mat4(1.0f), glm::vec3(0, 0, 0));
+	model = glm::rotate(model, glm::radians(45.0f), glm::vec3(0.0f, 0.0f, 1.0f));
+	model = glm::scale(model, glm::vec3(0.5, 13, 0.5));
+	projectionShader.setVec3("aColor", glm::vec3((float)100 / 255, (float)83 / 255, (float)148 / 255));
+	projectionShader.setMat4("model", model);
+	glDrawArrays(GL_TRIANGLES, 0, 36);
+
+	//Soporte2
+	model = savestate.at(0) = glm::translate(savestate.at(0) = glm::mat4(1.0f), glm::vec3(-13* cos(45* M_PI / 180)+ 0.25, 0, 0));
+	model = glm::rotate(model, glm::radians(45.0f), glm::vec3(0.0f, 0.0f, -1.0f));
+	model = glm::scale(model, glm::vec3(0.5, 13, 0.5));
+	projectionShader.setVec3("aColor", glm::vec3((float)100 / 255, (float)83 / 255, (float)148 / 255));
+	projectionShader.setMat4("model", model);
+	glDrawArrays(GL_TRIANGLES, 0, 36);
+
+	//Soporte3
+	model = savestate.at(0) = glm::translate(savestate.at(0) = glm::mat4(1.0f), glm::vec3(0, 0, -4));
+	model = glm::rotate(model, glm::radians(45.0f), glm::vec3(0.0f, 0.0f, 1.0f));
+	model = glm::scale(model, glm::vec3(0.5, 13, 0.5));
+	projectionShader.setVec3("aColor", glm::vec3((float)100 / 255, (float)83 / 255, (float)148 / 255));
+	projectionShader.setMat4("model", model);
+	glDrawArrays(GL_TRIANGLES, 0, 36);
+
+	//Soporte4
+	model = savestate.at(0) = glm::translate(savestate.at(0), glm::vec3(-13 * cos(45 * M_PI/180) + 0.25, 0, 0));
+	model = glm::rotate(model, glm::radians(45.0f), glm::vec3(0.0f, 0.0f, -1.0f));
+	model = glm::scale(model, glm::vec3(0.5, 13, 0.5));
+	projectionShader.setVec3("aColor", glm::vec3((float)100 / 255, (float)83 / 255, (float)148 / 255));
+	projectionShader.setMat4("model", model);
+	glDrawArrays(GL_TRIANGLES, 0, 36);
+
+	//Eje
+	model = glm::translate(savestate.at(0) = glm::mat4(1.0f), glm::vec3(-13 * cos(45 * M_PI / 180)/2, 13 * cos(45 * M_PI / 180) / 2, -2));
+	model = savestate.at(0) = glm::rotate(model, glm::radians(fortuna_rot), glm::vec3(0.0f, 0.0f, 1.0f));
+	model = glm::scale(model, glm::vec3(0.25, 0.25, 2.25));
+	projectionShader.setVec3("aColor", glm::vec3((float)100 / 255, (float)83 / 255, (float)148 / 255));
+	projectionShader.setMat4("model", model);
+	my_sphere.render();
+	glBindVertexArray(VAO);
+
+	for (int ind = 0; ind < 12; ind++)
+	{
+		//Viga30d
+		model = savestate.at(ind+1) = glm::rotate(savestate.at(0), glm::radians(ind*30.0f), glm::vec3(0.0f, 0.0f, 1.0f));
+		model = savestate.at(ind + 1) = glm::translate(savestate.at(ind + 1), glm::vec3(0, 8.5/2, 0));
+		model = glm::scale(model, glm::vec3(0.25, 8.5, 0.25));
+		projectionShader.setVec3("aColor", glm::vec3((float)100 / 255, (float)83 / 255, (float)148 / 255));
+		projectionShader.setMat4("model", model);
+		glDrawArrays(GL_TRIANGLES, 0, 36);
+		//cabina
+		model = glm::translate(savestate.at(ind + 1), glm::vec3(0,8.5/2,0));
+		model = savestate_adorno.at(ind) =glm::rotate(model, glm::radians(180.0f)-glm::radians(fortuna_rot)- glm::radians(ind*30.0f), glm::vec3(0.0f, 0.0f, 1.0f));
+		model = glm::scale(model, glm::vec3(0.5, 0.5, 0.5));
+		projectionShader.setVec3("aColor", glm::vec3((float)100 / 255, (float)83 / 255, (float)148 / 255));
+		projectionShader.setMat4("model", model);
+		my_sphere.render();
+		//adornos
+		model = glm::translate(savestate_adorno.at(ind), glm::vec3(0, 0.5+0.25, 0));
+		model = glm::scale(model, glm::vec3(0.25, 0.25, 0.25));
+		projectionShader.setVec3("aColor", glm::vec3((float)100 / 255, (float)83 / 255, (float)148 / 255));
+		projectionShader.setMat4("model", model);
+		my_sphere.render();
+		glBindVertexArray(VAO);
+
+	}
+
+	//torus, rueda para la fortuna huehuehueue
+	model = glm::scale(savestate.at(0), glm::vec3(2.0f, 2.0f, 0.5f));
+	//model = glm::translate(model, lightPos);
+	projectionShader.setMat4("model", model);
+	my_torus.render();
+	//torus
+
+
+	glBindVertexArray(lightVAO);
+	//lampShader.use();
+	//model = savestate.at(0) = glm::translate(savestate.at(0) = glm::mat4(1.0f), glm::vec3(0, 0, 0));
+	//lampShader.setMat4("projection", projection);
+	//lampShader.setMat4("view", view);
+	//lampShader.setMat4("model", model);
+	//glDrawArrays(GL_TRIANGLES, 0, 36);	//Light
+
+
+	//glBindVertexArray(0);
+
+}
+
+int main()
+{
+	// glfw: initialize and configure
+	// ------------------------------
+	glfwInit();
+	/*glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 4);
+	glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
+	glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);*/
+
+#ifdef __APPLE__
+	glfwWindowHint(GLFW_OPENGL_FORWARD_COMPAT, GL_TRUE); // uncomment this statement to fix compilation on OS X
+#endif
+
+	// glfw window creation
+	// --------------------
+	monitors = glfwGetPrimaryMonitor();
+	getResolution();
+
+	GLFWwindow* window = glfwCreateWindow(SCR_WIDTH, SCR_HEIGHT, "Practica 7", NULL, NULL);
+	if (window == NULL)
+	{
+		std::cout << "Failed to create GLFW window" << std::endl;
+		glfwTerminate();
+		return -1;
+	}
+	glfwSetWindowPos(window, 0, 30);
+	glfwMakeContextCurrent(window);
+	glfwSetFramebufferSizeCallback(window, resize);
+	glfwSetCursorPosCallback(window, mouse_callback);
+	glfwSetScrollCallback(window, scroll_callback);
+
+	//To Enable capture of our mouse
+	glfwSetInputMode(window, GLFW_CURSOR, GLFW_CURSOR_NORMAL);
+
+	glewInit();
+
+
+	//Mis funciones
+	//Datos a utilizar
+	myData();
+	my_torus.init();
+	my_sphere.init();
+
+	glEnable(GL_DEPTH_TEST);
+
+	// render loop
+	// While the windows is not closed
+	while (!glfwWindowShouldClose(window))
+	{
+		// per-frame time logic
+		// --------------------
+		double currentFrame = glfwGetTime();
+		deltaTime = currentFrame - lastFrame;
+		lastFrame = currentFrame;
+		// input
+		// -----
+		my_input(window);
+		animate();
+
+		// render
+		// Backgound color
+		glClearColor(0.0f, 0.0f, 0.0f, 1.0f);
+		glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
+
+		//Mi función de dibujo
+		display();
+
+		// glfw: swap buffers and poll IO events (keys pressed/released, mouse moved etc.)
+		// -------------------------------------------------------------------------------
+		glfwSwapBuffers(window);
+		glfwPollEvents();
+	}
+
+	// glfw: terminate, clearing all previously allocated GLFW resources.
+	// ------------------------------------------------------------------
+	glDeleteVertexArrays(1, &VAO);
+	glDeleteVertexArrays(1, &lightVAO);
+	glDeleteBuffers(1, &VBO);
+
+	glfwTerminate();
+	return 0;
+}
+
+// process all input: query GLFW whether relevant keys are pressed/released this frame and react accordingly
+// ---------------------------------------------------------------------------------------------------------
+void my_input(GLFWwindow *window)
+{
+	if (glfwGetKey(window, GLFW_KEY_ESCAPE) == GLFW_PRESS)
+		glfwSetWindowShouldClose(window, true);
+
+	if (glfwGetKey(window, GLFW_KEY_W) == GLFW_PRESS)
+		camera.ProcessKeyboard(FORWARD, (float)deltaTime);
+	if (glfwGetKey(window, GLFW_KEY_S) == GLFW_PRESS)
+		camera.ProcessKeyboard(BACKWARD, (float)deltaTime);
+	if (glfwGetKey(window, GLFW_KEY_A) == GLFW_PRESS)
+		camera.ProcessKeyboard(LEFT, (float)deltaTime);
+	if (glfwGetKey(window, GLFW_KEY_D) == GLFW_PRESS)
+		camera.ProcessKeyboard(RIGHT, (float)deltaTime);
+
+
+}
+
+// glfw: whenever the window size changed (by OS or user resize) this callback function executes
+// ---------------------------------------------------------------------------------------------
+void resize(GLFWwindow* window, int width, int height)
+{
+	// Set the Viewport to the size of the created window
+	glViewport(0, 0, width, height);
+}
+
+// glfw: whenever the mouse moves, this callback is called
+// -------------------------------------------------------
+void mouse_callback(GLFWwindow* window, double xpos, double ypos)
+{
+	if (firstMouse)
+	{
+		lastX = xpos;
+		lastY = ypos;
+		firstMouse = false;
+	}
+
+	double xoffset = xpos - lastX;
+	double yoffset = lastY - ypos; // reversed since y-coordinates go from bottom to top
+
+	lastX = xpos;
+	lastY = ypos;
+
+	camera.ProcessMouseMovement(xoffset, yoffset);
+}
+
+// glfw: whenever the mouse scroll wheel scrolls, this callback is called
+// ----------------------------------------------------------------------
+void scroll_callback(GLFWwindow* window, double xoffset, double yoffset)
+{
+	camera.ProcessMouseScroll(yoffset);
+}
